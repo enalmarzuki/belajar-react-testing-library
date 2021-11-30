646fa2a1b7889f3c72d1b905170cb1df85cfef69
enalmarzuki <40693134+enalmarzuki@users.noreply.github.com>
2021-11-30 14:23:05 +0800

    try mock function and fireEvent

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-it('should render same text passed into title props', () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.getByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
+describe('Header', () => {
+  it('should render same text passed into title props', () => {
+    render(<Header title="My Header" />);
+    const headingElement = screen.getByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 });
 
 // it('should render same text passed into title props', () => {
